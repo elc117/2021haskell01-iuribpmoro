3e59acb91fbcf724321547d9ee041eedf2f37aad
Iuri Bernardo Picolini Moro <>
2021-05-23 18:47:09 +0000

    Finished parte 3

--- haskell01parte3/svgSimple.hs
+++ haskell01parte3/svgSimple.hs
@@ -1,26 +1,39 @@
 import Text.Printf
 
 -- String inicial do SVG
+-- Recebe as medidas do height e width da imagem
 svgBegin :: Float -> Float -> String
 svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+
 
 -- String final do SVG
 svgEnd :: String
 svgEnd = "</svg>"
 
+
+-- Recebe cx, cy, r e fill
+-- cx e cy = coordenadas x e y do centro do circulo
+-- r = raio do circulo
+-- fill = cor de preenchimento
 svgCircle :: Int -> Int -> Int -> String -> String 
 svgCircle x y r style = 
   printf "<circle cx='%d' cy='%d' r='%d' fill='%s' />\n" x y r style
+
 
 -- Gera SVG com 2 círculos, um verde e um vermelho, com 0.4 de opacidade.
 -- A opacidade pode não ser suportada em alguns visualizadores de SVG.
 svgAll :: String
 svgAll = 
   svgBegin 500 500 ++ 
-  (svgCircle 60 60 50 "rgb(10, 145, 32, 0.4)") ++ 
-  (svgCircle 90 90 50 "rgb(105, 14, 30, 0.4)") ++ 
+  (svgCircle 75 75 75 "rgb(74, 132, 132)") ++ 
+  (svgCircle 425 75 75 "rgb(113, 190, 129)") ++ 
+  (svgCircle 250 250 100 "rgb(125, 176, 63)") ++ 
+  (svgCircle 75 425 75 "rgb(32, 123, 67)") ++ 
+  (svgCircle 425 425 75 "rgb(74, 245, 148)") ++ 
   svgEnd
 
+
+-- Gera o Circulo no arquivo circles.svg
 main :: IO ()
 main = do
-  writeFile "circles.svg" svgAll
+  writeFile "circle.svg" svgAll
